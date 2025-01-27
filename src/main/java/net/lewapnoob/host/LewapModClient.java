bb01c0e4ab2102df889514976492bb49ff7c56b4
Caliverx2 <mordaplutatv.yt@gmail.com>
2025-01-28 00:17:50 +0100

    Initial commit

---
+++ src/main/java/net/lewapnoob/host/LewapModClient.java
@@ -0,0 +1,10 @@
+package net.lewapnoob.host;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LewapModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
